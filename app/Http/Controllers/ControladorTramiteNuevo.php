e1f01919376b316e46ef37abcda4d0998802d683
djmaam <marcosarrietaarg@gmail.com>
2019-12-18 21:40:51 -0300

    arreglos 18/12 Marcos

--- app/Http/Controllers/ControladorTramiteNuevo.php
+++ app/Http/Controllers/ControladorTramiteNuevo.php
@@ -5,16 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Entidades\Sistema\Usuario;
 use App\Entidades\Sistema\Patente;
-use App\Entidades\Configuracion\Formulario;
+use App\Entidades\Tramite\Tramite;
 
 require app_path().'/start/constants.php';
 use Session;
 
-class ControladorConfiguracionFormularios extends Controller
+class ControladorTramiteNuevo extends Controller
     {
         public function index()
         {
-        $titulo = "Listado de Formularios";
+        $titulo = "Nuevo Trámite";
         if(Usuario::autenticado() == true)
         {
             if(!Patente::autorizarOperacion("MENUCONSULTA")) {
@@ -22,7 +22,7 @@ class ControladorConfiguracionFormularios extends Controller
                 $mensaje = "No tiene permisos para la operaci&oacute;n.";
                 return view ('sistema.pagina-error', compact('titulo', 'codigo', 'mensaje'));
             } else {
-                return view('configuracion.formulario-listar', compact('titulo'));
+                return view('configuracion.tramite-nuevo', compact('titulo'));
             }
         } else {
             return redirect('login');
@@ -65,3 +65,43 @@ class ControladorConfiguracionFormularios extends Controller
             return view("configuracion.formulario-nuevo", compact('titulo'));
         }
     
+        public function guardar(Request $request){
+        try {
+            //Define la entidad servicio
+            $titulo = "Modificar Formulario";
+            $entidad = new Formulario();
+            $entidad->cargarDesdeRequest($request);
+
+            //validaciones
+            if ($entidad->nombre == "") {
+                $msg["ESTADO"] = MSG_ERROR;
+                $msg["MSG"] = FALTANOMBRE; //ARREGLAR
+            } else {
+                if ($_POST["id"] > 0) {
+                    //Es actualizacion
+                    $entidad->guardar();
+
+                    $msg["ESTADO"] = MSG_SUCCESS;
+                    $msg["MSG"] = OKINSERT;
+                } else {
+                    //Es nuevo
+                    $entidad->insertar();
+
+                    $msg["ESTADO"] = MSG_SUCCESS;
+                    $msg["MSG"] = OKINSERT;
+                }
+                
+                return view('configuracion.formularios-listar', compact('titulo', 'msg'));
+            }
+        } catch (Exception $e) {
+            $msg["ESTADO"] = MSG_ERROR;
+            $msg["MSG"] = ERRORINSERT;
+        }
+
+        $id = $entidad->idformulario;
+        $formulario = new Formulario();
+        $formulario->obtenerPorId($id);
+
+        return view('configuracion.formulario-nuevo', compact('msg', 'menu', 'titulo', 'array_menu', 'array_menu_grupo')) . '?id=' . $formulario->idformulario;
+    }
+}
