ddddf4a914cea1cca7f535a6f43a1fc0cea3e6b1
ghmex <maxii.xd38@gmail.com>
2019-12-20 14:37:56 -0300

    cambios nuevos

--- app/Entidades/Tramite/Tramite.php
+++ app/Entidades/Tramite/Tramite.php
@@ -20,50 +20,8 @@ class Tramite extends Model
 
     ];
 
-<<<<<<< HEAD
+
     public function obtenerFiltradoEnProceso() {
-=======
-    
-    public function guardar() {
-        $sql = "UPDATE tramites SET
-                rectificativa='$this->rectificativa',
-                fecha_inicio='$this->fecha_inicio',
-                fk_idcliente='$this->fk_idcliente',
-                fk_idformulario='$this->fk_idformulario',
-                fk_idtramite_estado='$this->fk_idtramite_estado',
-                usuario_sistema='$this->usuario_sistema'
-            WHERE idtramite=?";
-        $affected = DB::update($sql, [$this->idtramite]);
-    }
-
-    public  function eliminar() {
-        $sql = "DELETE FROM tramites WHERE 
-            idtramite=?";
-        $affected = DB::delete($sql, [$this->idtramite]);
-    }
-
-    public function insertar() {
-        $sql = "INSERT INTO tramites (
-               rectificativa,
-               fecha_inicio,
-               fk_idcliente,
-               fk_idformulario,
-               fk_idtramite_estado,
-               usuario_sistema
-            ) VALUES (?, ?, ?, ?, ?, ?);";
-       $result = DB::insert($sql, [
-            $this->rectificativa, 
-            $this->fecha_inicio, 
-            $this->fk_idcliente, 
-            $this->fk_idformulario, 
-            $this->fk_idtramite_estado,
-            $this->usuario_sistema
-        ]);
-       return $this->idtramite = DB::getPdo()->lastInsertId();
-    }
-
-    public function obtenerFiltrado() {
->>>>>>> ad0b3840fddca593379127bb3c59dbd0f775a083
         $request = $_REQUEST;
         $columns = array(
            0 => 'C.nombre',
@@ -96,7 +54,7 @@ class Tramite extends Model
 
         return $lstRetorno;
     }
-        public function obtenerFiltradoBorrador() {
+    public function obtenerFiltradoBorrador() {
         $request = $_REQUEST;
         $columns = array(
            0 => 'C.nombre',
@@ -129,7 +87,7 @@ class Tramite extends Model
 
         return $lstRetorno;
     }
-            public function obtenerFiltradoIniciados() {
+    public function obtenerFiltradoIniciados() {
         $request = $_REQUEST;
         $columns = array(
            0 => 'C.nombre',
@@ -162,7 +120,7 @@ class Tramite extends Model
 
         return $lstRetorno;
     }
-                public function obtenerFiltradoFinalizados() {
+    public function obtenerFiltradoFinalizados() {
         $request = $_REQUEST;
         $columns = array(
            0 => 'C.nombre',
